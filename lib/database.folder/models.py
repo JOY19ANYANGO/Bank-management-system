c0af87e0dcb66292e53e1fdd6d46ef97f7393f5c
JOY19ANYANGO <joyanyango402@gmail.com>
2023-09-06 09:46:36 +0300

    generated transactions,customers and accounts tables

---
+++ lib/database.folder/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Float, create_engine, Date
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+engine = create_engine('sqlite:///bank.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, default=datetime.now)  # Use default=datetime.now
+    transaction_type = Column(String)
+    amount = Column(Float)
+    
+class Account(Base):
+    __tablename__ = "accounts"
+    id = Column(Integer, primary_key=True)
+    account_type = Column(String)
+    transaction_id = Column(Integer, ForeignKey('transactions.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
